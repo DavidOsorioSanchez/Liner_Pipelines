0dc1d7fd8e65efe37aac546bb6067218e3f8f90d
David Osorio <daalosx@gmail.com>
2024-07-11 09:18:04 -0500

    mirando haber

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,7 +3,8 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
     let points:number = 0;
+    return 'Hello World!';
+    
   }
 }
